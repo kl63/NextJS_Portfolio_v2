0443006fa793096eac5ec4478f7a5e5381e6d12a
kl63 <kl63@NJIT.edu>
2024-08-06 09:02:10 -0400

    added feature to search page BETA

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,64 +1,76 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from 'next/head';
+
 // Sections
-import GitRecentProjects from '../../components/projects/recent'
-import FeaturedProjects from '../../components/projects/featured'
+import GitRecentProjects from '../../components/projects/recent';
+import FeaturedProjects from '../../components/projects/featured';
+import Color from '../../components/utils/page.colors.util';
 
-import Color  from '../../components/utils/page.colors.util'
+import settings from '../../jsonData/layout/_settings.json';
+import colors from '../../jsonData/projects/_colors.json';
+import navbar from '../../jsonData/layout/navbar.json'; // Import your navbar.json
 
-import settings from '../../jsonData/layout/_settings.json'
-import colors from '../../jsonData/projects/_colors.json'
-
-//
 export default function Projects({ user, repos }) {
-	return (
-		<>
-		<Color colors={colors} />
-		<FeaturedProjects />
-		<GitRecentProjects user={user} repos={repos} />
-		</>
-	)
+    const router = useRouter();
+    
+    useEffect(() => {
+        const currentPath = router.pathname;
+        const currentPage = navbar.find(page => page.url === currentPath);
+    }, [router.pathname]);
+
+    return (
+        <>
+            <Head>
+                <title>{navbar.find(page => page.url === router.pathname)?.title || 'Default Title'}</title>
+            </Head>
+            <Color colors={colors} />
+            <FeaturedProjects />
+            <GitRecentProjects user={user} repos={repos} />
+        </>
+    );
 }
 
 // This gets called on every request
 export async function getServerSideProps({ res }) {
+    res.setHeader(
+        'Cache-Control',
+        'public, s-maxage=600, stale-while-revalidate=59'
+    );
 
-	res.setHeader(
-		'Cache-Control',
-		'public, s-maxage=600, stale-while-revalidate=59'
-	)
+    const [gitUserRes, gitReposRes] = await Promise.all([
+        fetch(`https://api.github.com/users/${settings.username.github}`),
+        fetch(`https://api.github.com/users/${settings.username.github}/repos`),
+    ]);
+    
+    let [user, repos] = await Promise.all([
+        gitUserRes.json(),
+        gitReposRes.json(),
+    ]);
 
-	const [ gitUserRes, gitReposRes] = await Promise.all( [
-		fetch(`https://api.github.com/users/${settings.username.github}`),
-		fetch(`https://api.github.com/users/${settings.username.github}/repos`),
-	] )
-	
-	let [ user, repos] = await Promise.all( [
-		gitUserRes.json(),
-		gitReposRes.json(), 
-	] )
+    if (user.login) {
+        user = [user].map(
+            ({ login, name, avatar_url, html_url }) => ({ login, name, avatar_url, html_url })
+        );
+    }
+    
+    if (repos.length) {
+        repos = repos.map(
+            ({ name, fork, description, forks_count, html_url, language, watchers, default_branch, homepage, pushed_at, topics }) => {
+                const timestamp = Math.floor(new Date(pushed_at) / 1000);
+                return ({ name, fork, description, forks_count, html_url, language, watchers, default_branch, homepage, timestamp, topics, pushed_at });
+            }
+        );
 
-	if (user.login) {
-		user = [user].map( 
-			({ login, name, avatar_url, html_url }) => ({ login, name, avatar_url, html_url })
-		)
-	}
-	
-	if (repos.length) {
-		repos = repos.map( 
-			({ name, fork, description, forks_count, html_url, language, watchers, default_branch, homepage, pushed_at, topics }) => {
-				const timestamp = Math.floor(new Date(pushed_at) / 1000)
-				return ({ name, fork, description, forks_count, html_url, language, watchers, default_branch, homepage, timestamp, topics, pushed_at })
-			}
-		)
+        repos.sort((a, b) => b.timestamp - a.timestamp);
 
-		repos.sort( (a, b) => b.timestamp - a.timestamp )
+        repos = repos.filter((e, i) => {
+            if (i < 8 && !e.topics.includes('github-config')) return e;
+            return false;
+        });
+    }
 
-		repos = repos.filter( (e, i) => {
-			if ( i < 8 && ! e.topics.includes('github-config')) return e
-			return false
-		})
-	}
+    if (!repos || !user) { return { notFound: true, } }
 
-	if (!repos || !user) { return { notFound: true,	} }
-
-	return { props: { repos, user } }
+    return { props: { repos, user } }
 }
--- pages/resume/index.jsx
+++ pages/resume/index.jsx
@@ -1,15 +1,26 @@
-import Color 	from '../../components/utils/page.colors.util'
-import colors 		from '../../jsonData/resume/_colors.json'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import Color from '../../components/utils/page.colors.util';
+import colors from '../../jsonData/resume/_colors.json';
 import Resume from '../../components/resume/resume';
+import navbar from '../../jsonData/layout/navbar.json'; // Import your navbar.json
 
-
-//
 export default function ResumePage({}) {
-	return (
-		<>	
-			<Color colors={colors} />	
-			<Resume />
-			
-		</>
-	)
+    const router = useRouter();
+    
+    useEffect(() => {
+        const currentPath = router.pathname;
+        const currentPage = navbar.find(page => page.url === currentPath);
+    }, [router.pathname]);
+
+    return (
+        <>
+            <Head>
+                <title>{navbar.find(page => page.url === router.pathname)?.title}</title>
+            </Head>
+            <Color colors={colors} />	
+            <Resume />
+        </>
+    );
 }
